2a8dad2d60349475ea19e98e426b57dab45ef812
Carter Thaxton <carter.thaxton@gmail.com>
2025-05-31 12:16:07 -0700

    Print in a loop

--- src/init/init.c
+++ src/init/init.c
@@ -10,11 +10,25 @@ int strlen(char* str) {
   return len;
 }
 
-int main() {
-  char *msg = "Welcome to Thaxtos!\n";
+void print(char *msg) {
   int len = strlen(msg);
-
   _syscall((void*) 1, msg, (void*) (long) len, 0, 0, 0, 64);
+}
+
+void delay(long ticks) {
+  for (long i=0; i < ticks; i++) {
+    // for now - just spin
+    ;
+  }
+}
+
+int main() {
+  print("Welcome to Thaxtos!\n");
+
+  while (1) {
+    delay(100000000);
+    print("TICK\n");
+  }
 
   return 69;
 }
