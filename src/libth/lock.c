48bf3c06f4a92f5551589a9abb81e06bf86ce603
Carter Thaxton <carter.thaxton@gmail.com>
2025-06-15 13:25:54 -0700

    Some improvements to lock/unlock

--- src/libth/lock.c
+++ src/libth/lock.c
@@ -2,20 +2,29 @@
 
 // TODO: use futexes - for now, these are simple spinlocks
 
+#ifndef __ATOMIC_HLE_ACQUIRE  // HLE (hardware lock-elision) is only defined on x86
+#define __ATOMIC_HLE_ACQUIRE 0
+#define __ATOMIC_HLE_RELEASE 0
+#endif
+
+// TODO: define this per arch
+#define __yield() asm("yield")
+
 void lock(bool* lock) {
+  // TTAS spinlock
   while (1) {
-    if (!__atomic_test_and_set(lock, __ATOMIC_ACQUIRE))
+    if (!__atomic_test_and_set(lock, __ATOMIC_ACQUIRE|__ATOMIC_HLE_ACQUIRE))
       return;
     while (__atomic_load_n(lock, __ATOMIC_RELAXED))
-      asm("yield");
+      __yield();
   }
 }
 
 bool try_lock(bool* lock) {
   return __atomic_load_n(lock, __ATOMIC_RELAXED) &&
-         __atomic_test_and_set(lock, __ATOMIC_ACQUIRE);
+         __atomic_test_and_set(lock, __ATOMIC_ACQUIRE|__ATOMIC_HLE_ACQUIRE);
 }
 
 void unlock(bool* lock) {
-  __atomic_store_n(lock, false, __ATOMIC_RELEASE);
+  __atomic_clear(lock, __ATOMIC_RELEASE|__ATOMIC_HLE_RELEASE);
 }
