27da48293b82fc4a9ab462f6b2a1c62d627f6bf3
Carter Thaxton <carter.thaxton@gmail.com>
2025-07-04 01:47:06 -0400

    Working hello EFI example using clang

--- src/efi_boot/build.sh
+++
@@ -1,5 +0,0 @@
-CFLAGS="-std=c17 -Wall -Wextra -Wpedantic -mgeneral-regs-only -ffreestanding -nostdlib"
-#LDFLAGS="-Wl,--subsystem,10 -e efi_main"
-LDFLAGS="-e efi_main"
-
-gcc $CFLAGS $LDFLAGS -o BOOTAA64.EFI efi_boot.c
---
+++ src/efi_boot/clean.sh
@@ -0,0 +1,2 @@
+rm -rf *.o *.d *.so *.efi
+
