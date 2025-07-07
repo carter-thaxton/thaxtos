07f1fde7cef871a897a4a2acccf2cca91678f6f9
Carter Thaxton <carter.thaxton@gmail.com>
2025-07-07 01:44:03 -0400

    Some minor changes to efi_boot.c before getting started

--- src/efi_boot/efi_boot.c
+++ src/efi_boot/efi_boot.c
@@ -5,8 +5,7 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
     (void)ImageHandle;  // Prevent compiler warning
 
     // Set text to yellow fg/ green bg
-    SystemTable->ConOut->SetAttribute(SystemTable->ConOut,
-            EFI_TEXT_ATTR(EFI_YELLOW,EFI_GREEN));
+    SystemTable->ConOut->SetAttribute(SystemTable->ConOut, EFI_TEXT_ATTR(EFI_YELLOW,EFI_GREEN));
 
     // Clear screen to bg color
     SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
@@ -14,16 +13,17 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
     SystemTable->ConOut->OutputString(SystemTable->ConOut, u"Hello, World!\r\n\r\n");
 
     // Set text to red fg/ black bg
-    SystemTable->ConOut->SetAttribute(SystemTable->ConOut,
-            EFI_TEXT_ATTR(EFI_RED,EFI_BLACK));
+    SystemTable->ConOut->SetAttribute(SystemTable->ConOut, EFI_TEXT_ATTR(EFI_RED,EFI_BLACK));
 
-    SystemTable->ConOut->OutputString(SystemTable->ConOut,
-            u"Press any key to shutdown...");
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, u"Press any key to shutdown...");
 
     // Wait until keypress, then return
     EFI_INPUT_KEY key;
-    while (SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &key) != EFI_SUCCESS)
-        ;
+    while (SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &key) != EFI_SUCCESS) ;
+
+    // Restore to white fg, black bg, and clear screen
+    SystemTable->ConOut->SetAttribute(SystemTable->ConOut, EFI_TEXT_ATTR(EFI_WHITE,EFI_BLACK));
+    SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
 
     // Shutdown, does not return
     SystemTable->RuntimeServices->ResetSystem(EfiResetShutdown, EFI_SUCCESS, 0, NULL);
