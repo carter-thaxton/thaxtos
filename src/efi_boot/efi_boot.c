135f0544c92941b24d5994a1407dff6580e72745
Carter Thaxton <carter.thaxton@gmail.com>
2025-07-14 09:24:56 -0700

    Working EFI bootloader that reads cmdline file

--- src/efi_boot/efi_boot.c
+++ src/efi_boot/efi_boot.c
@@ -1,20 +1,51 @@
 #include <efi/efi.h>
 
+EFI_GUID DevicePathProtocolGuid = EFI_DEVICE_PATH_PROTOCOL_GUID;
 EFI_GUID DevicePathUtilitiesProtocolGuid = EFI_DEVICE_PATH_UTILITIES_PROTOCOL_GUID;
 EFI_GUID DevicePathToTextProtocolGuid = EFI_DEVICE_PATH_TO_TEXT_PROTOCOL_GUID;
 EFI_GUID DevicePathFromTextProtocolGuid = EFI_DEVICE_PATH_FROM_TEXT_PROTOCOL_GUID;
 EFI_GUID LoadedImageDevicePathProtocolGuid = EFI_LOADED_IMAGE_DEVICE_PATH_PROTOCOL_GUID;
 EFI_GUID LoadedImageProtocolGuid = EFI_LOADED_IMAGE_PROTOCOL_GUID;
+EFI_GUID SimpleFileSystemProtocolGuid = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
 
 EFI_HANDLE ImageHandle;
 EFI_SYSTEM_TABLE* SystemTable;
 EFI_DEVICE_PATH_UTILITIES_PROTOCOL* DevicePathUtilitiesProtocol;
 EFI_DEVICE_PATH_TO_TEXT_PROTOCOL* DevicePathToTextProtocol;
 EFI_DEVICE_PATH_FROM_TEXT_PROTOCOL* DevicePathFromTextProtocol;
+EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* SimpleFileSystemProtocol;
+
+CONST EFI_DEVICE_PATH_PROTOCOL EndDevicePath = {
+    END_DEVICE_PATH_TYPE,
+    END_ENTIRE_DEVICE_PATH_SUBTYPE,
+    {
+        END_DEVICE_PATH_LENGTH,
+        0
+    }
+};
+
+CONST CHAR16* kernel_path = u"vmlinux";
+CONST CHAR16* cmdline_path = u"cmdline";
+
+
+void print(CONST CHAR16* str);
+
+void* memcpy(void* dst, const void* src, UINTN n) {
+    UINT8* d = (UINT8*) dst;
+    CONST UINT8* s = (CONST UINT8*) src;
+
+    while (n--) {
+        *d++ = *s++;
+    }
+    return d;
+}
 
 void* malloc(UINTN size) {
     void* result;
-    SystemTable->BootServices->AllocatePool(EfiConventionalMemory, size, &result);
+    EFI_STATUS status = SystemTable->BootServices->AllocatePool(EfiLoaderData, size, &result);
+    if (status != EFI_SUCCESS) {
+        print(u"Could not allocate memory!\r\n");
+    }
     return result;
 }
 
@@ -28,8 +59,12 @@ UINTN strlen(const CHAR16* str) {
     return len;
 }
 
-void print(CHAR16* str) {
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, str);
+void clear_screen() {
+    SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
+}
+
+void print(CONST CHAR16* str) {
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, (CHAR16*) str);
 }
 
 void print_device_path(EFI_DEVICE_PATH_PROTOCOL* devicePath) {
@@ -39,54 +74,51 @@ void print_device_path(EFI_DEVICE_PATH_PROTOCOL* devicePath) {
     free(devicePathString);
 }
 
+EFI_DEVICE_PATH_PROTOCOL* DevicePathFromHandle(EFI_HANDLE Device) {
+    EFI_DEVICE_PATH_PROTOCOL* DevicePath = NULL;
+    SystemTable->BootServices->HandleProtocol(Device, &DevicePathProtocolGuid, (void**)&DevicePath);
+    return DevicePath;
+}
+
+EFI_DEVICE_PATH_PROTOCOL* FileDevicePath(EFI_HANDLE Device, CONST CHAR16* FileName) {
+    FILEPATH_DEVICE_PATH      *FilePath;
+    EFI_DEVICE_PATH_PROTOCOL  *DevicePath;
+    EFI_DEVICE_PATH_PROTOCOL  *FileDevicePath;
+
+    DevicePath = NULL;
+
+    UINTN Size = (strlen(FileName) + 1) * sizeof(CHAR16);
+    FileDevicePath = malloc(Size + SIZE_OF_FILEPATH_DEVICE_PATH + END_DEVICE_PATH_LENGTH);
+    if (FileDevicePath != NULL) {
+        FilePath = (FILEPATH_DEVICE_PATH *) FileDevicePath;
+        FilePath->Header.Type    = MEDIA_DEVICE_PATH;
+        FilePath->Header.SubType = MEDIA_FILEPATH_DP;
+        memcpy(&FilePath->PathName, FileName, Size);
+        SetDevicePathNodeLength(&FilePath->Header, Size + SIZE_OF_FILEPATH_DEVICE_PATH);
+        SetDevicePathEndNode(NextDevicePathNode(&FilePath->Header));
+
+        if (Device != NULL) {
+            DevicePath = DevicePathFromHandle(Device);
+        }
+
+        DevicePath = DevicePathUtilitiesProtocol->AppendDevicePath(DevicePath, FileDevicePath);
+        free(FileDevicePath);
+    }
+
+    return DevicePath;
+}
+
 EFI_INPUT_KEY wait_for_key() {
     EFI_INPUT_KEY key;
     while (SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &key) != EFI_SUCCESS) ;
     return key;
 }
 
-EFI_STATUS launch_app(CHAR16* kernel, CHAR16* command_line) {
+EFI_STATUS launch_app(EFI_DEVICE_PATH_PROTOCOL* appDevicePath, CHAR16* command_line) {
     EFI_STATUS status;
-
-    // init protocols
-    status = SystemTable->BootServices->LocateProtocol(&DevicePathUtilitiesProtocolGuid, NULL, (void**)&DevicePathUtilitiesProtocol);
-    if (status != EFI_SUCCESS) return status;
-
-    status = SystemTable->BootServices->LocateProtocol(&DevicePathToTextProtocolGuid, NULL, (void**)&DevicePathToTextProtocol);
-    if (status != EFI_SUCCESS) return status;
-
-    status = SystemTable->BootServices->LocateProtocol(&DevicePathFromTextProtocolGuid, NULL, (void**)&DevicePathFromTextProtocol);
-    if (status != EFI_SUCCESS) return status;
-
-    // Get device path referring to current image
-    EFI_DEVICE_PATH_PROTOCOL* currentDevicePath;
-    status = SystemTable->BootServices->HandleProtocol(ImageHandle, &LoadedImageDevicePathProtocolGuid, (void**)&currentDevicePath);
-    if (status != EFI_SUCCESS) return status;
-
-    // Make device path referring to kernel
-    CHAR16* devicePathString = DevicePathToTextProtocol->ConvertDevicePathToText(currentDevicePath, FALSE, TRUE);
-
-    // find ) from end of string
-    UINTN len = strlen(devicePathString);
-    UINTN i;
-    for (i = len; devicePathString[i] != u')'; i--) ;
-    i++;
-
-    // append path to kernel
-    for (int j=0; kernel[j] != u'\0'; ) {
-        devicePathString[i++] = kernel[j++];
-    }
-    devicePathString[i++] = u'\0';
-
-    EFI_DEVICE_PATH_PROTOCOL* appDevicePath = DevicePathFromTextProtocol->ConvertTextToDevicePath(devicePathString);
-
-    // print(u"Modified device path: ");
-    // print_device_path(appDevicePath);
-    // print(u"\r\n");
 
     // Load app image
     EFI_HANDLE appImageHandle;
-
     status = SystemTable->BootServices->LoadImage(TRUE, ImageHandle, appDevicePath, NULL, 0, &appImageHandle);
     if (status != EFI_SUCCESS) return status;
 
@@ -94,7 +126,7 @@ EFI_STATUS launch_app(CHAR16* kernel, CHAR16* command_line) {
     status = SystemTable->BootServices->HandleProtocol(appImageHandle, &LoadedImageProtocolGuid, (void**)&appImageProtocol);
     if (status != EFI_SUCCESS) return status;
 
-    // Set linux command-line arguments
+    // Set command-line arguments
     appImageProtocol->LoadOptions = command_line;
     appImageProtocol->LoadOptionsSize = strlen(command_line) * sizeof(CHAR16);
 
@@ -107,19 +139,70 @@ EFI_STATUS launch_app(CHAR16* kernel, CHAR16* command_line) {
 
 // EFI Image Entry Point
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE* systemTable) {
+    EFI_STATUS status;
 
     // init globals
     ImageHandle = imageHandle;
     SystemTable = systemTable;
 
-    // Clear screen
-    SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
+    // init protocols
+    status = SystemTable->BootServices->LocateProtocol(&DevicePathUtilitiesProtocolGuid, NULL, (void**)&DevicePathUtilitiesProtocol);
+    if (status != EFI_SUCCESS) return status;
 
-    // Launch app, returns on error, or if app exits
-    CHAR16* kernel = u"/vmlinux";
-    CHAR16* command_line = u"root=/dev/vda2 rw quiet";
+    status = SystemTable->BootServices->LocateProtocol(&DevicePathToTextProtocolGuid, NULL, (void**)&DevicePathToTextProtocol);
+    if (status != EFI_SUCCESS) return status;
 
-    if (launch_app(kernel, command_line) != EFI_SUCCESS) {
+    status = SystemTable->BootServices->LocateProtocol(&DevicePathFromTextProtocolGuid, NULL, (void**)&DevicePathFromTextProtocol);
+    if (status != EFI_SUCCESS) return status;
+
+    // Get root ESP filesystem, where curent image is loaded
+    EFI_LOADED_IMAGE_PROTOCOL* currentImage;
+    status = SystemTable->BootServices->HandleProtocol(ImageHandle, &LoadedImageProtocolGuid, (void**)&currentImage);
+    if (status != EFI_SUCCESS) return status;
+
+    EFI_HANDLE rootDevice = currentImage->DeviceHandle;
+    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* fileSystemProtocol;
+    status = SystemTable->BootServices->HandleProtocol(rootDevice, &SimpleFileSystemProtocolGuid, (void**)&fileSystemProtocol);
+    if (status != EFI_SUCCESS) return status;
+
+    EFI_FILE_PROTOCOL* rootDir;
+    status = fileSystemProtocol->OpenVolume(fileSystemProtocol, &rootDir);
+    if (status != EFI_SUCCESS) return status;
+
+    // load cmdline file into buffer
+    CHAR16 command_line_16[1024];
+    command_line_16[0] = u'\0';
+
+    EFI_FILE_PROTOCOL* cmdlineFile;
+    status = rootDir->Open(rootDir, &cmdlineFile, (CHAR16*)cmdline_path, EFI_FILE_MODE_READ, 0);
+    if (status == EFI_SUCCESS) {
+        CHAR8 command_line[512];
+        UINTN command_line_size = sizeof(command_line);
+        status = cmdlineFile->Read(cmdlineFile, &command_line_size, command_line);
+
+        if (status == EFI_SUCCESS) {
+            // convert to UTF-16
+            UINTN j;
+            for (j=0; command_line[j] != '\0' && command_line[j] != '\n'; j++) {
+                command_line_16[j] = command_line[j];
+            }
+            command_line_16[j] = u'\0';
+        }
+    }
+
+    // create path to kernel
+    EFI_DEVICE_PATH_PROTOCOL* kernelDevicePath = FileDevicePath(rootDevice, kernel_path);
+
+    clear_screen();
+    print(u"Launching: ");
+    print_device_path(kernelDevicePath);
+    print(u" ");
+    print(command_line_16);
+    print(u"\r\n");
+    // wait_for_key();
+
+    // Launch kernel, returns on error, or if app exits
+    if (launch_app(kernelDevicePath, command_line_16) != EFI_SUCCESS) {
         print(u"App failed to launch\r\n\r\n");
     }
 
--- src/efi_boot/make.sh
+++ src/efi_boot/make.sh
@@ -9,7 +9,7 @@ elif [ "$1" = "install" ]; then
   cp efi_boot.efi ../../root/boot/EFI/BOOT/BOOTAA64.EFI
 else
   INCLUDE="-I/usr/include -I/usr/include/aarch64-linux-gnu/"
-  CFLAGS="-ffreestanding -MMD -mno-red-zone -std=c17 -target aarch64-unknown-windows -Wall -Werror -pedantic $INCLUDE"
+  CFLAGS="-ffreestanding -MMD -mno-red-zone -std=c17 -target aarch64-unknown-windows -Wall -Werror -pedantic -Wno-pointer-to-int-cast $INCLUDE"
   LDFLAGS="-flavor link -subsystem:efi_application -entry:efi_main"
 
   clang $CFLAGS -c efi_boot.c -o efi_boot.o
