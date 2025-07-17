4a8c67301559c41f353fbd6f5936fbc802608ea8
Carter Thaxton <carter.thaxton@gmail.com>
2025-07-17 13:31:34 -0700

    Simplify run-disk.sh to run.sh, and prepend underscores to the others, for convenience

--- bin/run.sh
+++ bin/run.sh
@@ -6,7 +6,7 @@ qemu-system-aarch64 \
   -drive if=pflash,format=raw,file=boot/QEMU_VARS.fd \
   -drive if=none,format=raw,file=disk.img,id=hd \
   -device virtio-blk-pci,drive=hd \
-  -serial stdio \
+  -serial mon:stdio \
   -nographic \
   -nodefaults \
   $@
